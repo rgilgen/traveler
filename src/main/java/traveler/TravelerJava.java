bd5d6da5459661c86262a7f45fd7ea99a645c68f
rgilgen <rgilgen@live.com>
2023-05-26 13:36:46 +0200

    Drei Testfälle Korrekt

--- src/main/java/traveler/TravelerJava.java
+++ src/main/java/traveler/TravelerJava.java
@@ -3,14 +3,20 @@ package traveler;
 import java.util.List;
 
 public class TravelerJava implements Traveler {
-
-    public static void main(String[] args) {
-
-    }
-
     @Override
     public Journey choose_best_sum(int maxDistance, int cityVisits, List<Integer> listOfCityDistances) {
-        return new Journey(List.of(new City(0, 50)));
-
+        boolean isCityInRange = listOfCityDistances.stream().filter(d -> d.intValue() <= maxDistance).count() > 0;
+        if (isCityInRange) {
+            City city = new City(0, 0);
+            for (int i = 0; i < listOfCityDistances.size(); i++) {
+                Integer currentDistance = listOfCityDistances.get(i);
+                if ((currentDistance <= maxDistance) &&
+                        (currentDistance > city.getDistance())) {
+                    city = new City(i, currentDistance);
+                }
+            }
+            return new Journey(List.of(city));
+        }
+        return null;
     }
 }
